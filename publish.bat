ee5f27bf3f14d4e1049593c6f6cf2a5c548c883c
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-03-07 15:53:30 +0800

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -1,5 +1,7 @@
-echo "enter you nuget package name(such as package_name, not package_name.nupkg)"
+@echo off
+
+echo enter you nuget package name(such as package_name, not package_name.nupkg)
 set /p pkgname =
-echo "enter you nuget api"
+echo enter you nuget api
 set /p api =
 nuget push -Source https://api.nuget.org/v3/index.json %pkgname%.nupkg -ApiKey %api%
