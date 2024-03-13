2afec57dcf67c8e8f21e62352afbd9a0ab97cd09
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-03-13 22:43:15 +0800

    Tools.Shell.Async 1.0.0 (#24)
    * Tools.Shell.Async

---
+++ Tools/Tools.Shell.Async/ShellExecutorAsync.cs
@@ -0,0 +1,40 @@
+﻿namespace Tools.Shell.Async
+{
+    public class ShellExecutorAsync
+    {
+        public class Cmd
+        {
+            public class User
+            {
+                public static Task<string> ExecuteCmdCommandAsync(string command, bool redirectOutput = true, bool useShellExecute = false, bool createNoWindow = true)
+                {
+                    return Task.Run(() => ShellExecutor.Cmd.User.ExecuteCmdCommand(command, redirectOutput, useShellExecute, createNoWindow));
+                }
+            }
+            public class Admin
+            {
+                public static Task<string> ExecuteCmdCommandAsync(string command, bool redirectOutput = true, bool useShellExecute = false, bool createNoWindow = true)
+                {
+                    return Task.Run(() => ShellExecutor.Cmd.Admin.ExecuteCmdCommand(command, redirectOutput, useShellExecute, createNoWindow));
+                }
+            }
+        }
+        public class PowerShell
+        {
+            public class User
+            {
+                public static async Task<string> ExecutePowerShellCommandAsync(string command, bool redirectOutput = true, bool useShellExecute = false, bool createNoWindow = true)
+                {
+                    return await Task.Run(() => ShellExecutor.PowerShell.User.ExecutePowerShellCommand(command, redirectOutput, useShellExecute, createNoWindow));
+                }
+            }
+            public class Admin
+            {
+                public static async Task<string> ExecutePowerShellCommandAsAdminAsync(string command, bool redirectOutput = true, bool useShellExecute = false, bool createNoWindow = true)
+                {
+                    return await Task.Run(() => ShellExecutor.PowerShell.Admin.ExecutePowerShellCommandAsAdmin(command, redirectOutput, useShellExecute, createNoWindow));
+                }
+            }
+        }
+    }
+}
---
+++ Tools/Tools.Shell.Async/readme.md
@@ -0,0 +1,44 @@
+﻿# Tools.Shell.Async
+
+Run Tools.Shell asynchronously.
+
+Attention:
+- 1.0.0 and later is for .NET 8.0
+
+## Usage
+
+### Cmd
+
+#### User
+
+```c#
+string command = "your_command_here";
+string result = await ShellExecutorAsync.Cmd.User.ExecuteCmdCommandAsync(command);
+Console.WriteLine(result);
+```
+
+#### Admin
+
+```c#
+string command = "your_command_here";
+string result = await ShellExecutorAsync.Cmd.Admin.ExecuteCmdCommandAsync(command);
+Console.WriteLine(result);
+```
+
+### PowerShell
+
+#### User
+
+```c#
+string command = "your_command_here";
+string result = await ShellExecutorAsync.PowerShell.User.ExecuteCmdCommandAsync(command);
+Console.WriteLine(result);
+```
+
+#### Admin
+
+```c#
+string command = "your_command_here";
+string result = await ShellExecutorAsync.PowerShell.Admin.ExecuteCmdCommandAsync(command);
+Console.WriteLine(result);
+```
