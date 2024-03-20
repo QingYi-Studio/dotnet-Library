cdba5990d9fd003c1efeedd62b9523cbc942d9e6
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-03-20 18:12:23 +0800

    Hotspot (#34)
    * localserver
    
    * readme
    
    * Hotspot
    
    * readme

---
+++ Server/Server.Hotspot/Close.vb
@@ -0,0 +1,10 @@
+﻿Public Class Close
+    Public Shared Sub StopHotspot()
+        Dim stopHotspot As New ProcessStartInfo With {
+            .FileName = "netsh",
+            .Arguments = "wlan stop hostednetwork",
+            .WindowStyle = ProcessWindowStyle.Hidden
+        }
+        Process.Start(stopHotspot)
+    End Sub
+End Class
