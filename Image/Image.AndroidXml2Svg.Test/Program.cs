2dab6dac0e594dafeb0f02c5d042a4eb4c54abb7
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-04-21 16:38:31 +0800

    完善测试代码

--- Image/Image.AndroidXml2Svg.Test/Program.cs
+++ Image/Image.AndroidXml2Svg.Test/Program.cs
@@ -1,3 +1,5 @@
 ﻿using Image.AndroidXml2Svg;
 
 StartConvert.Start("TestFile/5a.output.xml", "TestFile/5a.test-output.svg");
+
+// await StartConvert.StartAsync("TestFile/5a.output.xml", "TestFile/5a.test-output.svg");
