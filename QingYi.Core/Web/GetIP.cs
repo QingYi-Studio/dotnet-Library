5cef2f49f7035cb26d7e6b9cdb27dd25611f76bf
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-05-01 13:23:14 +0800

    Core (#1)
    * GetTempFolder
    
    * create folder
    
    * display/hide folder
    
    * create folder in temp
    
    * 注释
    
    * create file
    
    * create file
    
    * BugFix
    
    * Create Format.cs
    
    * 连接测试
    
    * 完成测试模块
    
    * Update QingYi.Core.csproj
    
    * Create GetIP.cs

---
+++ QingYi.Core/Web/GetIP.cs
@@ -0,0 +1,87 @@
+﻿using System.Net;
+
+namespace QingYi.Core.Web
+{
+    public class GetIP
+    {
+        /// <summary>
+        /// Get IP Address<br></br>
+        /// 获取IP地址
+        /// </summary>
+        /// <param name="url">Url|链接</param>
+        /// <returns>IP</returns>
+        /// <exception cref="Exception"></exception>
+        public static string GetIPAddress(string url)
+        {
+            try
+            {
+                IPAddress[] addresses = Dns.GetHostAddresses(new Uri(url).Host);
+                foreach (IPAddress address in addresses)
+                {
+                    return address.ToString();
+                }
+                return "IP not found";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get IPv4 Address<br></br>
+        /// 获取IPv4地址
+        /// </summary>
+        /// <param name="url">Url|链接</param>
+        /// <returns>IP</returns>
+        /// <exception cref="Exception"></exception>
+        // 获取IPv4地址
+        public static string GetIPv4Address(string url)
+        {
+            try
+            {
+                IPAddress[] addresses = Dns.GetHostAddresses(new Uri(url).Host);
+                foreach (IPAddress address in addresses)
+                {
+                    if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        return address.ToString();
+                    }
+                }
+                return "IP not found";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get IPv6 Address<br></br>
+        /// 获取IPv6地址
+        /// </summary>
+        /// <param name="url">Url|链接</param>
+        /// <returns>IP</returns>
+        /// <exception cref="Exception"></exception>
+        // 获取IPv6地址
+        public static string GetIPv6Address(string url)
+        {
+            try
+            {
+                IPAddress[] addresses = Dns.GetHostAddresses(new Uri(url).Host);
+                foreach (IPAddress address in addresses)
+                {
+                    if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+                    {
+                        return address.ToString();
+                    }
+                }
+                return "IP not found";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ QingYi.Core/Web/Test.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+
+namespace QingYi.Core.Web
+{
+    public class Test
+    {
+        /// <summary>
+        /// Sync Test<br></br>
+        /// 同步测试
+        /// </summary>
+        /// <param name="url">Url|网址</param>
+        /// <returns>Test result|测试结果</returns>
+        /// <exception cref="Exception"></exception>
+        public static double TestConnection(string url)
+        {
+            // 发送请求并记录延迟
+            DateTime startTime = DateTime.Now;
+
+            try
+            {
+                using HttpClient client = new();
+                HttpResponseMessage response = client.GetAsync("http://" + url).Result;
+                response.EnsureSuccessStatusCode(); // 确保成功响应
+
+                // 请求结束时间
+                DateTime endTime = DateTime.Now;
+
+                // 计算连接延迟
+                TimeSpan delay = endTime - startTime;
+                return delay.TotalMilliseconds;
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Async Test<br></br>
+        /// 异步测试
+        /// </summary>
+        /// <param name="url">Url|网址</param>
+        /// <returns>Test result|测试结果</returns>
+        /// <exception cref="Exception"></exception>
+        public static async Task<double> TestConnectionAsync(string url)
+        {
+            // 发送请求并记录延迟
+            DateTime startTime = DateTime.Now;
+
+            try
+            {
+                using HttpClient client = new();
+                HttpResponseMessage response = await client.GetAsync("http://" + url);
+                response.EnsureSuccessStatusCode(); // 确保成功响应
+
+                // 请求结束时间
+                DateTime endTime = DateTime.Now;
+
+                // 计算连接延迟
+                TimeSpan delay = endTime - startTime;
+                return delay.TotalMilliseconds;
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
