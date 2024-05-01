00e801897d7649f7d8b7416d1c93cd7aeb9168f0
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-05-01 14:33:30 +0800

    单文件下载完成

---
+++ QingYi.Tools/QingYi.Tools.Download/Default/Core/SingleFile.cs
@@ -0,0 +1,51 @@
+﻿namespace QingYi.Tools.Download.Default.Core
+{
+    public class SingleFile
+    {
+        internal static void Download(string url, string destinationPath)
+        {
+            using var client = new HttpClient();
+            try
+            {
+                var response = client.GetAsync(url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    using var responseStream = response.Content.ReadAsStreamAsync().Result;
+                    using var fileStream = File.Create(destinationPath);
+                    responseStream.CopyTo(fileStream);
+                }
+                else
+                {
+                    throw new Exception($"Failed to download file. Status code: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error downloading file: {ex.Message}");
+            }
+        }
+
+        internal static async Task DownloadAsync(string url, string destinationPath)
+        {
+            using var client = new HttpClient();
+            try
+            {
+                using var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    using var fileStream = File.Create(destinationPath);
+                    await response.Content.CopyToAsync(fileStream);
+                }
+                else
+                {
+                    throw new Exception($"Failed to download file. Status code: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error downloading file: {ex.Message}");
+            }
+        }
+    }
+}
