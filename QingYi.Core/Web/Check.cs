8354f5b878fafc40d5231aa3d2d7480da1644ef6
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-05-01 14:02:03 +0800

    sync (#2)
    * WPF.MultiLanguage
    
    * Create Translate.sln
    
    * Delete Translate.sln
    
    * color conversion
    
    * Audio.Crack.NetEase.Auto
    
    * targz
    
    * more support
    
    * image
    
    * Create CreateFile.cs
    
    * Update Convert.cs
    
    * 添加异步
    
    * update setting
    
    * Create StartConvert.cs
    
    * doc
    
    * test
    
    * viewBox转换失败问题修复
    
    * 异步代码修复
    
    * 完善测试代码
    
    * change pkg id
    
    * add license
    
    * change id again
    
    * delete icon
    
    * delete image
    
    * delete share proxy
    
    * readme
    
    * Update readme.md
    
    * delete tools
    
    * add tools
    
    * Core (#1)
    
    * GetTempFolder
    
    * create folder
    
    * display/hide folder
    
    * create folder in temp
    
    * 注释
    
    * create file
    
    * create file
    
    * BugFix
    
    * Create Format.cs
    
    * 连接测试
    
    * 完成测试模块
    
    * Update QingYi.Core.csproj
    
    * Create GetIP.cs
    
    * update readme
    
    * 添加网络快速测试代码
    
    仅测试是否可正常连接
    
    * Delete publish.bat

---
+++ QingYi.Core/Web/Check.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace QingYi.Core.Web
+{
+    public class Check
+    {
+        public static double CheckWebsite(string url)
+        {
+            // 格式化URL，去除协议和路径
+            string formattedUrl = Format.GetHost(url);
+
+            // 发起四次连接测试，并记录延迟
+            double totalDelay = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                totalDelay += Test.TestConnection(formattedUrl);
+            }
+
+            // 计算平均延迟
+            double averageDelay = totalDelay / 4;
+
+            return averageDelay;
+        }
+
+        public static async Task<double> CheckWebsiteAsync(string url)
+        {
+            // 格式化URL，去除协议和路径
+            string formattedUrl = Format.GetHost(url);
+
+            // 发起四次连接测试，并记录延迟
+            double totalDelay = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                totalDelay += await Test.TestConnectionAsync(formattedUrl);
+            }
+
+            // 计算平均延迟
+            double averageDelay = totalDelay / 4;
+
+            return averageDelay;
+        }
+    }
+}
