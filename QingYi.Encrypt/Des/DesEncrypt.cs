aa825f46f04d512f7ec12adfe184003b5bc310ce
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-02 18:13:11 +0800

    ECB

---
+++ QingYi.Encrypt/Des/DesEncrypt.cs
@@ -0,0 +1,22 @@
+﻿namespace QingYi.Encrypt.Des
+{
+    public class DesEncrypt(string key)
+    {
+        public void TextEncrypt(string text)
+        {
+            // 将密钥转换为字节数组
+            byte[] keyBytes = Encoding.UTF8.GetBytes(Key.IfKeyEmpty(key));
+
+            // 调用ECB.Text.Encrypt方法
+            ECB.Text.Encrypt(text, keyBytes);
+        }
+
+        public void FileEncrypt(string inputFile, string outputFile)
+        {
+            // 将密钥转换为字节数组
+            byte[] keyBytes = Encoding.UTF8.GetBytes(Key.IfKeyEmpty(key));
+
+            ECB.File.EncryptFile(inputFile, outputFile, keyBytes);
+        }
+    }
+}
---
+++ QingYi.Encrypt/Test.cs
@@ -0,0 +1,14 @@
+﻿using QingYi.Encrypt.Des;
+
+namespace QingYi.Encrypt
+{
+    class Test
+    {
+        static void DesTest()
+        {
+            string key = "1234";
+            DesEncrypt des = new(key);
+            des.TextEncrypt("1234");
+        }
+    }
+}
