06009199c9e0c629a553772eb5bc9609b64557f1
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-02 18:41:43 +0800

    optimize the usage again

--- QingYi.Encrypt/Test.cs
+++ QingYi.Encrypt/Test.cs
@@ -1,15 +1,14 @@
-﻿using QingYi.Encrypt.Des;
-
-namespace QingYi.Encrypt
+﻿namespace QingYi.Encrypt
 {
     class Test
     {
         static void DesTest()
         {
             string key = "1234";
-            DesEncrypt des = new DesEncrypt(key, Mode.ECB);
-            des.TextEncrypt("1234");
-
+            Encrypt.DesEncrypt desEcb = new(key, Encrypt.DesEncryptMode.ECB);
+            Encrypt.DesEncrypt desCbc = new(key, Encrypt.DesEncryptMode.CBC);
+            desEcb.TextEncrypt("1234");
+            desCbc.TextEncrypt("1234");
         }
     }
 }
