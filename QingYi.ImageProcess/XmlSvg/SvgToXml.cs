d57e097ec831bcd4659d0eb75b586b345f43a9ea
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-09 14:20:52 +0800

    xml2svg

---
+++ QingYi.ImageProcess/XmlSvg/SvgToXml.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QingYi.ImageProcess.XmlSvg
+{
+    internal class SvgToXml
+    {
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,8 @@
+﻿using QingYi.ImageProcess.XmlSvg;
+
+XmlToSvg xmlToSvg = new()
+{
+    InputFilePath = "ic_launcher_foreground.xml",
+    OutputFilePath = "output.svg"
+};
+xmlToSvg.Convert();
