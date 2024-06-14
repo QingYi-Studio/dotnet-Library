fcf175cd05ef67d60a2f651dd36cd8304ad090c8
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-14 10:56:29 +0800

    Axml-IntReader (#54)
    * IntReader
    
    * IntReader

---
+++ QingYi.AXML/AXmlResourceParser/Parser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AXmlResourceParser
+{
+    public class Parser
+    {
+        private IntReader.IntReader m_reader;
+        private bool m_operational = false;
+    }
+}
