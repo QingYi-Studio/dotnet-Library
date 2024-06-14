647b0f65a6e1a121814f6dc0142fd7dd62c0b3bd
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-14 10:55:38 +0800

    IntReader

--- QingYi.AXML/AXmlResourceParser/Parser.cs
+++ QingYi.AXML/AXmlResourceParser/Parser.cs
@@ -2,8 +2,9 @@
 
 namespace AXmlResourceParser
 {
-    public class Class1
+    public class Parser
     {
-
+        private IntReader.IntReader m_reader;
+        private bool m_operational = false;
     }
 }
