eeac9b28c993cc05358a2f6565ea40b0a4c26b3d
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-14 10:48:12 +0800

    IntReader

---
+++ QingYi.AXML/AXML/GetXml.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace AXML
+{
+    public class GetXml
+    {
+        private readonly bool addXmlHead = true;
+        private readonly string inputFilePath;
+        private readonly string outputFilePath;
+
+        public GetXml(bool addXmlHead, string inputFilePath, string outputFilePath)
+        {
+            this.addXmlHead = addXmlHead;
+            this.inputFilePath = inputFilePath;
+            this.outputFilePath = outputFilePath;
+        }
+
+        public async Task<string> GetAsync()
+        {
+            // TODO
+            return null;
+        }
+    }
+}
