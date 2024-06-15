8d51902f3d382d1ad47a3b53d838fd6f0e536aae
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-15 22:31:27 +0800

    QingYi.AXML.Exception.V1 (#5)
    * IntReader
    
    * IntReader
    
    * Axml-IntReader (#54)
    
    * IntReader
    
    * IntReader
    
    * publish
    
    * publish intreader (#55)
    
    * IntReader
    
    * IntReader
    
    * publish
    
    * create proj
    
    * ChunkUtil
    
    * Axml.chunkutil (#56)
    
    * IntReader
    
    * IntReader
    
    * publish
    
    * create proj
    
    * ChunkUtil
    
    * string block
    
    * NamespaceStack
    
    * Xml Resource Parser
    
    * Typed Value
    
    * delete old proj
    
    * Create QingYi.AXML.Android.csproj
    
    * Create IntReader.cs
    
    * Create TypedValue.cs
    
    * Create StringBlock.cs
    
    * namespace stack
    
    * 同步命名空间
    
    * 纠正命名空间
    
    * Create ChunkUtil.cs
    
    * Update NamespaceStack.cs
    
    * 同步命名空间
    
    * Update QingYi.AXML.sln
    
    * Update QingYi.AXML.Android.csproj
    
    * Create XmlResourceParser.cs
    
    * Create AttributeSet.cs
    
    * Create XmlPullParserException.cs
    
    * Update XmlResourceParser.cs
    
    * bug fix
    
    * Create XmlPullParser.cs
    
    * 修正目录名称
    
    * RuntimeException
    
    ---------
    
    Co-authored-by: QingYi Studio <146829069+QingYi-Studio@users.noreply.github.com>

---
+++ QingYi.AXML/Android/Content/XmlResourceParser.cs
@@ -0,0 +1,12 @@
+﻿using QingYi.AXML.Android.Util;
+using QingYi.AXML.Android.XmlPull.V1;
+
+namespace QingYi.AXML.Android.Content
+{
+    // 定义一个接口，继承自 XmlPullParser 和 AttributeSet
+    public interface IXmlResourceParser : IXmlPullParser, IAttributeSet
+    {
+        // 在接口中声明 close() 方法
+        void Close();
+    }
+}
