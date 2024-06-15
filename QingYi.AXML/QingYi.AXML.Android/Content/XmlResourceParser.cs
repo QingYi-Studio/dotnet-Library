92c2ad99becf6432016df21f100cadfec35d2e8a
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-15 14:37:25 +0800

    Create XmlResourceParser.cs

---
+++ QingYi.AXML/QingYi.AXML.Android/Content/XmlResourceParser.cs
@@ -0,0 +1,9 @@
+﻿namespace QingYi.AXML.Android.Content
+{
+    // 定义一个接口，继承自 XmlPullParser 和 AttributeSet
+    public interface IXmlResourceParser : XmlPullParser, AttributeSet
+    {
+        // 在接口中声明 close() 方法
+        void Close();
+    }
+}
