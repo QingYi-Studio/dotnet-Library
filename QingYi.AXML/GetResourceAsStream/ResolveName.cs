aeaaebc52bacf6f8dea561ee838b2e36b4adeb74
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-16 15:27:36 +0800

    Create ResolveName.cs

---
+++ QingYi.AXML/GetResourceAsStream/ResolveName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace QingYi.AXML.GetResourceAsStream
+{
+    internal class ResolveName
+    {
+        public string Resolve(string name)
+        {
+            if (!name.StartsWith("/"))
+            {
+                Type c = IsArray() ? ElementType() : this.GetType();
+                string baseName = c.Namespace;
+                if (!string.IsNullOrEmpty(baseName))
+                {
+                    name = baseName.Replace('.', '/') + "/" + name;
+                }
+            }
+            else
+            {
+                name = name.Substring(1);
+            }
+            return name;
+        }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern bool IsArray();
+
+        private Type ElementType()
+        {
+            if (!IsArray()) return null;
+
+            Type c = GetType(); // Equivalent to typeof(MyClass) if this method is within MyClass
+
+            while (c.IsArray)
+            {
+                c = c.GetElementType();
+            }
+
+            return c;
+        }
+    }
+}
