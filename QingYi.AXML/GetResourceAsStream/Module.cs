6e87fee88ccea83e9e5ed3158fcb515cebf3542c
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-16 15:27:26 +0800

    Create Module.cs

---
+++ QingYi.AXML/GetResourceAsStream/Module.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QingYi.AXML.GetResourceAsStream
+{
+    internal class Module
+    {
+        [NonSerialized]
+        private Module module;
+
+        public Module GetModule() { return module; }
+    }
+}
