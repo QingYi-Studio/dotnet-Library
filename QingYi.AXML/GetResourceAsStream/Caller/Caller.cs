6aec88c6f555472ca58ced48c56f54fb539b5ff0
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-16 15:27:20 +0800

    Create Caller.cs

---
+++ QingYi.AXML/GetResourceAsStream/Caller/Caller.cs
@@ -0,0 +1,11 @@
+﻿namespace QingYi.AXML.GetResourceAsStream.Caller
+{
+    internal class Caller
+    {
+        public static Module GetModule()
+        {
+            Module moduleN = new Module();
+            return moduleN.GetModule();
+        }
+    }
+}
