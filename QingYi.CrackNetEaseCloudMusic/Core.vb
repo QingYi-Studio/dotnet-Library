64eafdc5e6cb3cb5e17283b99116e0adeafb1d7e
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-07-08 15:34:39 +0800

    1.0.0beta

---
+++ QingYi.CrackNetEaseCloudMusic/Core.vb
@@ -0,0 +1,54 @@
+Imports System.Runtime.InteropServices
+
+Public Class Core
+
+    Private Const DLL_PATH As String = "lib/libncmdump.dll"
+    Private ReadOnly NeteaseCryptClass As IntPtr = IntPtr.Zero
+
+    <DllImport(DLL_PATH, CallingConvention:=CallingConvention.Cdecl)>
+    Private Shared Function CreateNeteaseCrypt(path As IntPtr) As IntPtr
+    End Function
+
+    <DllImport(DLL_PATH, CallingConvention:=CallingConvention.Cdecl)>
+    Private Shared Function Dump(NeteaseCrypt As IntPtr) As Integer
+    End Function
+
+    <DllImport(DLL_PATH, CallingConvention:=CallingConvention.Cdecl)>
+    Private Shared Sub FixMetadata(NeteaseCrypt As IntPtr)
+    End Sub
+
+    <DllImport(DLL_PATH, CallingConvention:=CallingConvention.Cdecl)>
+    Private Shared Sub DestroyNeteaseCrypt(NeteaseCrypt As IntPtr)
+    End Sub
+
+    ''' <summary>
+    ''' 创建 NeteaseCrypt 类的实例。
+    ''' </summary>
+    ''' <param name="FileName">网易云音乐 ncm 加密文件路径</param>
+    Public Sub New(FileName As String)
+        NeteaseCryptClass = CreateNeteaseCrypt(Marshal.StringToHGlobalAnsi(FileName))
+    End Sub
+
+    ''' <summary>
+    ''' 启动转换过程。
+    ''' </summary>
+    ''' <returns>返回一个整数，指示转储过程的结果。如果成功，返回0；如果失败，返回1。</returns>
+    Public Function Dump() As Integer
+        Return Dump(NeteaseCryptClass)
+    End Function
+
+    ''' <summary>
+    ''' 修复音乐文件元数据。
+    ''' </summary>
+    Public Sub FixMetadata()
+        FixMetadata(NeteaseCryptClass)
+    End Sub
+
+    ''' <summary>
+    ''' 销毁 NeteaseCrypt 类的实例。
+    ''' </summary>
+    Public Sub Destroy()
+        DestroyNeteaseCrypt(NeteaseCryptClass)
+    End Sub
+
+End Class
---
+++ QingYi.CrackNetEaseCloudMusic/Crack.vb
@@ -0,0 +1,17 @@
+﻿Public Class Crack
+
+    Public Shared Sub CrackAudio(ByVal filePath As String)
+        ' 创建 NeteaseCrypt 类的实例
+        Dim neteaseCrypt As New Core(filePath)
+
+        ' 启动转换过程
+        Dim result As Integer = neteaseCrypt.Dump()
+
+        ' 修复元数据
+        neteaseCrypt.FixMetadata()
+
+        ' [务必]销毁 NeteaseCrypt 类的实例
+        neteaseCrypt.Destroy()
+    End Sub
+
+End Class
