0e8e3afdc0bb440913bdc6f65a7554093ff435b8
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-07-19 23:42:45 +0800

    saved games

---
+++ QingYi.Core/Folder/SavedGames/CreateFolder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace QingYi.Core.Folder.SavedGames
+{
+    public class CreateFolder
+    {
+        public static string Create(string folderName)
+        {
+            // 在 Saved Games 文件夹内创建一个名为 "MyGameFolder" 的子文件夹
+            string newFolderPath = Path.Combine(GetSavedGamesFolder.Get(), folderName);
+
+            try
+            {
+                Directory.CreateDirectory(newFolderPath);
+
+                return newFolderPath;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
